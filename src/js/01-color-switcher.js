7d039a7df41ffe537b649af1de369145cdafa6c2
Serhii Lapchenko <lapchenko.ss@gmail.com>
2022-09-08 23:43:11 +0300

    conplete task#1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
 
+startBtn.addEventListener('click', onStartBtnClick);
+stopBtn.addEventListener('click', onStopBtnClick);
+
+stopBtn.setAttribute('disabled', '');
+
+function onStartBtnClick() {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+
+  stopBtn.removeAttribute('disabled', '');
+  startBtn.setAttribute('disabled', '');
+}
+
+function onStopBtnClick() {
+  clearInterval(timerId);
+  startBtn.removeAttribute('disabled', '');
+  stopBtn.setAttribute('disabled', '');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
