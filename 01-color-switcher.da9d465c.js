e50bcecdf4bb383127f267fe3c7f16656ef3992e
Slapchenko <103333806+Slapchenko@users.noreply.github.com>
2022-09-14 19:54:57 +0000

    Deploying to gh-pages from @ Slapchenko/goit-js-hw-09@d79525633a2c54a0a32a67361084ab06e8d7ef96 🚀

---
+++ 01-color-switcher.da9d465c.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),r=document.querySelector("body");let d=null;t.addEventListener("click",(function(o){d=setInterval((()=>{r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),e.removeAttribute("disabled"),t.setAttribute("disabled","")})),e.addEventListener("click",(function(r){clearInterval(d),t.removeAttribute("disabled"),e.setAttribute("disabled","")})),e.setAttribute("disabled","");
+//# sourceMappingURL=01-color-switcher.da9d465c.js.map
